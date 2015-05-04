3dd8a39651d5c3818305a5b86cdad568f435aac9
hhassan1230 <hhassan@oswego.edu>
2015-05-04 11:09:29 -0400

    2nd?

--- transform.rb
+++ transform.rb
@@ -1,3 +1,11 @@
 def transform(obj)
-
+	# binding.pry
+	next_hash = {}
+	obj.each do |key, val|
+		val.each do |thing|
+			giv = thing.downcase
+			next_hash["#{giv}"] ||= key
+		end
+	end
+	next_hash
 end
